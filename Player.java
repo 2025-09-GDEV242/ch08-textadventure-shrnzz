c84d3ebb64800dda921d38257048edd275a706d1
shrnzz <114313101+shrnzz@users.noreply.github.com>
2025-11-16 14:04:42 -0500

    added player class

--- Player.java
+++ Player.java
@@ -1,32 +1,72 @@
 /**
- * Write a description of class Player here.
+ * Player class for the text adventure game. 
+ * Creates a player that can travel to different rooms and have items in their
+ * inventory.
  *
  * @author Sahar Naz
  * @version 2026.11.16
  */
-public class Player
-{
-    // instance variables - replace the example below with your own
-    private int x;
+public class Player {
+
+    private Room currentRoom; // the room the player is currently in
+    private Item inventory;   // the item the player is carrying (can be null)
 
     /**
-     * Constructor for objects of class Player
+     * Constructor for Player.
+     * @param startRoom The room where the player starts.
      */
-    public Player()
-    {
-        // initialise instance variables
-        x = 0;
+    public Player(Room startRoom) {
+        currentRoom = startRoom;
+        inventory = null; // player starts with no item
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
+     * Get the current room of the player.
+     * @return The current room.
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+    /**
+     * Set the current room of the player.
+     * @param room The room to move to.
+     */
+    public void setCurrentRoom(Room room) {
+        currentRoom = room;
+    }
+
+    /**
+     * Get the item the player is carrying.
+     * @return The item, or null if none.
+     */
+    public Item getInventory() {
+        return inventory;
+    }
+
+    /**
+     * Pick up an item.
+     * @param item The item to pick up.
+     */
+    public void pickUp(Item item) {
+        inventory = item;
+    }
+
+    /**
+     * Drop the item the player is carrying.
+     * @return The item dropped, or null if none.
+     */
+    public Item dropItem() {
+        Item temp = inventory;
+        inventory = null;
+        return temp;
+    }
+
+    /**
+     * Check if the player has an item.
+     * @return true if the player has an item, false otherwise.
+     */
+    public boolean hasItem() {
+        return inventory != null;
     }
 }
